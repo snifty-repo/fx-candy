e0a4a425080f307b145586622d097f028c80a5e5
Lucas Hugdahl <lucashugdahl@Lucass-MacBook-Air.local>
2022-02-07 23:12:03 -0800

    feat: works

---
+++ config.js
@@ -0,0 +1,70 @@
+const fxCandyConfig = [
+    {
+        folder : "images/Background",
+        values : [
+            {
+                file : "Base"
+            }
+        ]
+    },
+    {
+        folder : "images/Eyes",
+        values : [
+            {
+                file : "Bottom Right"
+            },
+            {
+                file : "Cartoon Eyes"
+            },
+            {
+                file : "Center"
+            },
+            {
+                file : "Crossed"
+            },
+            {
+                file : "Normal Eyes"
+            },
+            {
+                file : "Top Right"
+            },
+        ]
+    },
+    {
+        folder : "images/Glasses",
+        values : [
+            {
+                file : "Green"
+            },
+            {
+                file : "Pink"
+            },
+            {
+                file : "Yellow"
+            },
+        ]
+    },
+    {
+        folder : "images/Mouth",
+        values : [
+            {
+                file : "Bleh"
+            },
+            {
+                file : "Fine"
+            },
+            {
+                file : "Lips"
+            },
+            {
+                file : "Open"
+            },
+            {
+                file : "Smile"
+            },
+            {
+                file : "Tongue"
+            },
+        ]
+    },
+]
---
+++ generator.js
@@ -0,0 +1,112 @@
+(async () => {
+    // The first two characters of the hash is always "oo".
+    const SEGMENT_OFFSET = 2;
+
+    // The length of each attribute hash segment.
+    const SEGMENT_LENGTH = 3;
+
+    // Char code for "1"
+    const HASH_SUM_MIN = 49 * SEGMENT_LENGTH;
+
+     // Char code for "z"
+    const HASH_SUM_MAX = 122 * SEGMENT_LENGTH;
+
+    window.$fxhashFeatures = {};
+
+    const generate = () => fxCandyConfig.map((attribute, idx) => {
+        const nextHashIndex = (idx * SEGMENT_LENGTH) + SEGMENT_OFFSET;
+
+        const hashSegment = fxhash.substring(nextHashIndex, nextHashIndex + SEGMENT_LENGTH);
+
+        let hashSum = 0;
+        let hashCharacterCodes = [];
+
+        for(let i = 0; i < hashSegment.length; i++) {
+            const characterCode = hashSegment.charCodeAt(i);
+
+            hashSum += characterCode;
+            hashCharacterCodes[i] = characterCode;
+        }
+
+        // A value represented as a percentage that's between the min hash sum and max hash sum.
+        const rarityScore = ((hashSum - HASH_SUM_MIN) * 100) / (HASH_SUM_MAX - HASH_SUM_MIN);
+
+        // Default to the first item 
+        let [ value ] = attribute.values;
+        
+        if(attribute.values.length > 0) {
+            const valueIndexFromRarityScore = Math.round(attribute.values.length - ((rarityScore / 100) * attribute.values.length));
+
+            // Because 5.5 will round up to 6 which might not be an index.
+            const valueIndex = valueIndexFromRarityScore < attribute.values.length ? valueIndexFromRarityScore : valueIndexFromRarityScore - 1;
+
+            value = attribute.values[valueIndex];
+        }
+
+        const {
+            name   : attributeName = "",
+            folder : attributeFolder = "",
+        } = attribute;
+
+        const {
+            name  : valueName = "",
+            file  : valueFile = "",
+        } = value;
+
+        const getLastPathSegment = (path) => path.split("/").reverse()[0];
+        const cleanPathSlashes = (str) => str.replace(/\/$/, "");
+        const buildValueFilePath = (folder, file) => `./${cleanPathSlashes(folder)}/${cleanPathSlashes(file)}.png`;
+
+        const result = {
+            attribute : "",
+            value     : {},
+        };
+
+        // Determine attribute name
+        result.attribute = attributeName ? attributeName : getLastPathSegment(attributeFolder);
+
+        // Determine value name
+        result.value.file = valueName ?
+            buildValueFilePath(attributeFolder, valueName) :
+            buildValueFilePath(attributeFolder, getLastPathSegment(valueFile));
+
+        result.value.name = valueName ? valueName : getLastPathSegment(valueFile);
+        
+        return result;
+    });
+
+    const metadata = generate();
+
+    const canvas = document.createElement("canvas");
+    const canvasContext = canvas.getContext("2d");
+
+    const onLoad = (image) => new Promise((resolve, reject) => {
+        image.onload = resolve;
+    });
+
+    for(let i = 0; i < metadata.length; i++) {
+        const image = new Image();
+
+        const waitForLoad = new Promise((resolve, reject) => {
+            image.onload = resolve;
+        });
+
+        image.src = metadata[i].value.file;
+
+        await waitForLoad;
+
+        canvasContext.drawImage(image, 0, 0, canvas.width, canvas.height);
+
+        window.$fxhashFeatures[metadata[i].attribute] = metadata[i].value.name;
+    }
+    
+    const cavasImgSrc = canvas.toDataURL("image/png");
+    
+    document.write(`<img src="${cavasImgSrc}" width="1080" height="1080"/>`);
+
+    console.log(JSON.stringify({
+        input : fxhash,
+        output : metadata,
+        $fxhashFeatures : window.$fxhashFeatures,
+    }, null, 4));
+})();
